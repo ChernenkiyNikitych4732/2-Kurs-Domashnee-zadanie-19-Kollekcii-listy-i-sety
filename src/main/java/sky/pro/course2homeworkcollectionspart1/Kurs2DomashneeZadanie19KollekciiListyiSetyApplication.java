1643c76891442b0b27d19d87c2a91872068e6c9c
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-08-03 19:56:27 +0100

    2 Курс ДЗ №19 Коллекции: листы и сеты

---
+++ src/main/java/sky/pro/course2homeworkcollectionspart1/Kurs2DomashneeZadanie19KollekciiListyiSetyApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course2homeworkcollectionspart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2DomashneeZadanie19KollekciiListyiSetyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2DomashneeZadanie19KollekciiListyiSetyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/course2homeworkcollectionspart1/Kurs2DomashneeZadanie19KollekciiListyISetyApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.course2homeworkcollectionspart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2DomashneeZadanie19KollekciiListyISetyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
